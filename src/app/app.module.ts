9a462437db0cf41c007021ef4e219d84392755cf
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-11-08 19:12:49 +0200

    started the info section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './home/main/main.component';
+import { TitleComponent } from './home/main/section-info/title/title.component';
+import { SectionInfoComponent } from './home/main/section-info/section-info.component';
+import { ButtonComponent } from './home/main/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TitleComponent,
+    SectionInfoComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
