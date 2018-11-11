1e9a3304400a45cf88939599a257cade1216ace1
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-11-11 21:28:04 +0200

    hover functionality on social links

--- src/app/global/components/footer/footer.component.ts
+++ src/app/global/components/footer/footer.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+    facebookImgSrc = '/assets/facebook-logo-white.svg';
+    linkedinImgSrc = '/assets/linkedin-logo-white.svg';
+    instagramImgSrc = '/assets/instagram-logo-white.svg';
+    githubImgSrc = '/assets/github-logo-white.png';
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onMouseOver(socialApp) {
+        switch (socialApp) {
+            case 'facebook':
+                this.facebookImgSrc = '/assets/facebook-logo.svg';
+                break;
+            case 'linkedin':
+                this.linkedinImgSrc = '/assets/linkedin-logo.svg';
+                break;
+            case 'instagram':
+                this.instagramImgSrc = '/assets/instagram-logo.svg';
+                break;
+            case 'github':
+                this.githubImgSrc = '/assets/github-logo.png';
+                break;
+        }
+    }
+
+    onMouseOut(socialApp) {
+        switch (socialApp) {
+            case 'facebook':
+                this.facebookImgSrc = '/assets/facebook-logo-white.svg';
+                break;
+            case 'linkedin':
+                this.linkedinImgSrc = '/assets/linkedin-logo-white.svg';
+                break;
+            case 'instagram':
+                this.instagramImgSrc = '/assets/instagram-logo-white.svg';
+                break;
+            case 'github':
+                this.githubImgSrc = '/assets/github-logo-white.png';
+                break;
+        }
+    }
 }
