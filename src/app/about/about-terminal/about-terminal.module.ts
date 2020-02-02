64800a3c7b0c6bf4b9ade8d7e4c99d09c7428d14
Andy <eduard-andrei.radulescu@bawagpsk.com>
2020-02-02 17:22:49 +0100

    refactor about page

---
+++ src/app/about/about-terminal/about-terminal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AboutMainComponent } from '../about-main/about-main.component';
+
+@NgModule({
+  declarations: [
+    AboutMainComponent
+  ],
+  exports: [
+    AboutMainComponent
+  ]
+})
+export class AboutTerminalModule {
+}
